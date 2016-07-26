35b13fbe26dfddf4c2427c4fafbbfd1f19899193
Ashley Taylor <ashleytaylor14@gmail.com>
2016-07-26 16:04:21 +0100

    remove product and image

--- controller/delete.js
+++ controller/delete.js
@@ -1,3 +1,4 @@
+var path = require('path');
 var s3 = require('app-util').s3();
 var Product = require(global.__products_base + '/models/product');
 
@@ -9,14 +10,25 @@ var remove = function onDelete(req, res, next) {
 
 	var query = { _id: id, user: req.user._id };
 
-	Product.remove(query, function onRemove(err, result) {
+	Product.findAndRemove(query, function onRemove(err, product) {
 		if (err) return next(err);
 
-		s3.delete([{ Key: '/products/' + req.params.product + '/' + req.params.id }], function onDelete(err, result) {
+		var ext = path.extname(product.image);
+
+		s3.delete([{ Key: 'products/' + req.params.product + '/' + req.params.id + ext }], function onDelete(err, result) {
 			if (err) return next(err);
 			return res.status(200).json(result);
 		});
 	});
+
+	// Product.remove(query, function onRemove(err, result) {
+	// 	if (err) return next(err);
+
+	// 	s3.delete([{ Key: 'products/' + req.params.product + '/' + req.params.id + '' }], function onDelete(err, result) {
+	// 		if (err) return next(err);
+	// 		return res.status(200).json(result);
+	// 	});
+	// });
 };
 
 module.exports = remove;
