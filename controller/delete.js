67fabf13f390cadeec43027f1f1aa286bfe799ef
Ashley Taylor <ashleytaylor14@gmail.com>
2016-07-26 16:10:40 +0100

    remove image on product

--- controller/delete.js
+++ controller/delete.js
@@ -12,6 +12,7 @@ var remove = function onDelete(req, res, next) {
 
 	Product.findOneAndRemove(query, function onRemove(err, product) {
 		if (err) return next(err);
+		if (!product) return next(new Error('No products found'));
 
 		var ext = path.extname(product.image);
 
@@ -20,15 +21,6 @@ var remove = function onDelete(req, res, next) {
 			return res.status(200).json(result);
 		});
 	});
-
-	// Product.remove(query, function onRemove(err, result) {
-	// 	if (err) return next(err);
-
-	// 	s3.delete([{ Key: 'products/' + req.params.product + '/' + req.params.id + '' }], function onDelete(err, result) {
-	// 		if (err) return next(err);
-	// 		return res.status(200).json(result);
-	// 	});
-	// });
 };
 
 module.exports = remove;
