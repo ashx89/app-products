670d280ae3b074f4461887689823b5f670fed7ec
Ashley Taylor <ashleytaylor14@gmail.com>
2016-07-28 22:26:19 +0100

    fetch any single product if an id is present

--- controller/fetch.js
+++ controller/fetch.js
@@ -2,9 +2,7 @@ var _ = require('underscore');
 var Product = require(global.__products_base + '/models/product');
 
 var fetch = function onFetch(req, res, next) {
-	var query = { user: req.user._id };
-
-	(req.params.id) ? _.extend(query, { _id: req.params.id }) : {};
+	var query = (req.params.id) ? { _id: req.params.id } : { user: req.user._id };
 
 	Product.find(query, function onFind(err, doc) {
 		if (err) return next(err);
