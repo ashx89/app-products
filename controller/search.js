c01129f144427f28d9986ce4e8d7c1d10ced9c67
Ashley Taylor <ashleytaylor14@gmail.com>
2016-07-28 18:10:19 +0100

    reduced search

--- controller/search.js
+++ controller/search.js
@@ -11,17 +11,7 @@ var search = function onSearch(req, res, next) {
 		sort: req.query.sort || 'title'
 	};
 
-	if (req.query.type) {
-		opts.query = _.extend(opts.query, { type: req.query.type });
-	}
-
-	if (req.query.id) {
-		opts.query = _.extend(opts.query, { _id: req.query.id });
-	}
-
-	if (req.query.user) {
-		opts.query = _.extend(opts.query, { user: req.query.user });
-	}
+	opts.query = _.extend(opts.query, req.query);
 
 	require('app-search')(opts).runSearch(function onSearch(err, result) {
 		return res.status(200).json(result);
