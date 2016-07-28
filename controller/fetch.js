35b1840aaf8a0ea94a84524ffa66f97b88662505
Ashley Taylor <ashleytaylor14@gmail.com>
2016-07-28 14:06:11 +0100

    allow fetching of other products

--- controller/fetch.js
+++ controller/fetch.js
@@ -8,9 +8,7 @@ var Product = require(global.__products_base + '/models/product');
 var fetch = function onFetch(req, res, next) {
 	var id = req.params.id;
 
-	var query = { user: req.user._id };
-
-	if (id) query = _.extend(query, { _id: id });
+	var query = (id) ? _.extend(query, { _id: id }) : {};
 
 	Product.find(query, function onFind(err, doc) {
 		if (err) return next(err);
