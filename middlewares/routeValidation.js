72fe2bae19168bc6189cd0cffbec6da15796b4d6
Ashley Taylor <ashleytaylor14@gmail.com>
2016-07-28 22:02:28 +0100

    updated product checking

--- middlewares/routeValidation.js
+++ middlewares/routeValidation.js
@@ -1,3 +1,3 @@
 module.exports = function onCheckProductParameter(req, res, next) {
-	return (process.env.APPLICATION_PRODUCTS.split(',').indexOf(req.params.product) === -1) || next();
+	return (process.env.APPLICATION_PRODUCTS.split(',').indexOf(req.params.product) === -1) ? res.status(404).json({ title: 'Resource not found', status: 404, path: req.path }) : next();
 };
