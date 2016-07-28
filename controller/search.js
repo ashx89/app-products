05e792b0cdac84b2baf009fa60969e9f9c088b20
Ashley Taylor <ashleytaylor14@gmail.com>
2016-07-28 18:15:13 +0100

    reduced search code

--- controller/search.js
+++ controller/search.js
@@ -1,17 +1,12 @@
 var _ = require('underscore');
 
-/**
- * Search products
- */
 var search = function onSearch(req, res, next) {
 	var opts = {
 		req: req,
-		query: {},
+		query: _.extend({}, req.query),
 		model: require(global.__base + '/manager').ProductModel,
 		sort: req.query.sort || 'title'
 	};
-
-	opts.query = _.extend(opts.query, req.query);
 
 	require('app-search')(opts).runSearch(function onSearch(err, result) {
 		return res.status(200).json(result);
